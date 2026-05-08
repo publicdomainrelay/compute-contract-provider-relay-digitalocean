e0eed4e272554c9b1fa5d5c0a4c2e76a83d87b77
John Andersen <johnandersenpdx@gmail.com>
2026-05-07 21:40:50 -0700

    feat: in line with spec
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- src/compute_contract_provider_relay_digitalocean/server.py
+++ src/compute_contract_provider_relay_digitalocean/server.py
@@ -210,15 +210,44 @@ class ATProtoRecordRef(BaseModel):
     cid: str
 
 
+class CCRFP_v0_0_0_Location(BaseModel):
+    country: str
+    region: str
+
+
 class CCRFP_v0_0_0(BaseModel):
     mem: str
     cpus: int
     disk: str
     network: str
+    location: CCRFP_v0_0_0_Location
+    user_data: str
 
 
 class CCB_v0_0_0_Embed(BaseModel):
     record: ATProtoRecordRef
+
+
+class CCB_v0_0_0_Bid_x402(BaseModel):
+    base_url: str
+
+
+class CCB_v0_0_0_Bid_WIF_Subject(BaseModel):
+    format: str
+
+
+class CCB_v0_0_0_Bid_WIF(BaseModel):
+    issuer_uri: str
+    subject: CCB_v0_0_0_Bid_WIF_Subject
+
+
+class CCB_v0_0_0_Bid(BaseModel):
+    cost: float
+    currency: str
+    frequency: str
+    prepay: bool
+    x402: CCB_v0_0_0_Bid_x402
+    wif: CCB_v0_0_0_Bid_WIF
 
 
 class CCB_v0_0_0(BaseModel):
